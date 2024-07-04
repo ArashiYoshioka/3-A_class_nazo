14a7025ac6228c2a0ec604a49fea18f3a1f25626
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-05 03:35:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const result = document.getElementById('result');
 
     if (answerInput === "解答") {
-        result.textContent = "おめでとう!";
+        result.textContent = "おめでとう!https://cdn2.me-qr.com/pdf/fe806c32-5751-41be-a219-666c20e29b4f.pdf";
     } else if (answerInput === "真実") {
         result.textContent = "完全解決!　ありがとう!";
     } else {
