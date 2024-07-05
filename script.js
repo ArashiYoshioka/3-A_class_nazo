78ec5c23748c4324cf3b15e1bca1c7038c2826dc
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-05 19:13:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function checkAnswer() {
 
     if (answerInput === "解答") {
         result.textContent = "https://arashiyoshioka.github.io/3-A-zaseki/";
-    } else if (answerInput === "真実") {
+    } else if (answerInput === "真相") {
         result.textContent = "完全解決!　ありがとう!";
     } else {
         result.textContent = "残念...";
