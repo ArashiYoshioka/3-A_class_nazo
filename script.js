d6f8c5fa976c94d5bea8d9f746107858b17c132a
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-06 00:55:53 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkAnswer() {
     const result = document.getElementById('result');
 
     if (answerInput === "解答") {
-        result.textinnerHTML = <a href= "https://arashiyoshioka.github.io/3-A-zaseki/" >座席表　（７月５日～）</a>
+        result.innerHTML = <a href= "https://arashiyoshioka.github.io/3-A-zaseki/" >座席表　（７月５日～）</a>
     } else if (answerInput === "真相") {
         result.textContent = "完全解決!　ありがとう!";
     } else {
