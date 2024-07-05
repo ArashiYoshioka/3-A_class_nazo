32b1f7b8d0f09375319ef46d7c1e9d69e5548639
ArashiYoshioka <220808a34@hokkaido-c.ed.jp>
2024-07-06 01:08:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function checkAnswer() {
     const result = document.getElementById('result');
 
     if (answerInput === "解答") {
-        result.innerHTML = '<a href="https://arashiyoshioka.github.io/3-A-zaseki/" target="_blank">座席表　（７月５日～）</a>';
-    } else if (answerInput === "真相") {
+        result.textContent = "おめでとう!";
+    } else if (answerInput === "真実") {
         result.textContent = "完全解決!　ありがとう!";
     } else {
         result.textContent = "残念...";
